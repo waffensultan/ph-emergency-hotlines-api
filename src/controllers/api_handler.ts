14d4f06a08c51f76d5295d85ab73c601518882f3
Waffen Sultan <waffen.sultan@gmail.com>
2024-10-25 17:34:16 +0800

    feat: changes and fixes

--- src/controllers/api_handler.ts
+++ src/controllers/api_handler.ts
@@ -3,18 +3,19 @@ import type { Request, Response } from "express";
 import fs from 'fs';
 import path from 'path';
 
+
 const api_handler = (req: Request, res: Response) => {
+    const { region, province, city_or_municipality, } = req.params;
+    const { hotline_type } = req.query;
+
     try {
         const file_path = path.resolve(__dirname, '../models/source.json');
         const source = JSON.parse(fs.readFileSync(file_path, 'utf-8'))[0];
 
-        const { region, province, city_or_municipality, } = req.params;
-        const { hotline_type } = req.query;
-
         const keys = [region, province, city_or_municipality].filter(Boolean);
         const response = get_nested_data(source, keys);
 
-        if (response.length >= 1) {
+        if (response) {
             res.json({
                 status: 'success',
                 data: (!hotline_type ? response : response[hotline_type.toString()])
@@ -37,5 +38,7 @@ const api_handler = (req: Request, res: Response) => {
 const get_nested_data = (data: any, keys: any) => {
     return keys.reduce((acc: any, key: any) => (acc && acc[key] ? acc[key] : null), data);
 }
+
+/* REMEMBER TO ADD STATUS CODES */
 
 export default api_handler;
--- src/routes/api_router.ts
+++ src/routes/api_router.ts
@@ -10,18 +10,9 @@ api_router.get('/', (_, res: Response) => {
     res.json({
         message: "Welcome to the Philippines Emergency Hotlines API!",
         example: "Usage: /api/{region}/{province}/{city_or_municipality}?hotline_type={hotline_type}",
-        note: "Hotline types are: General, Police, Medical, Rescue/Disaster, Earthquake/Seismic, Traffic",
-        additional_note: "Some locations might not have every type of emergency hotline."
+        hotline_types: "general, police, medical, rescue_disaster, earthquake_seismic, traffic, social, and digital",
+        note: "Please understand that some locations might not have every type of emergency hotline available."
     })
 })
 
 export default api_router;
-
-// - https://api.phemergencyhotlines.com/{region}/{province}/{city or municipality}/{barangay}/?hotline_type={hotline_type}
-// - result: https://api.phemergencyhotlines.com/cavite/alfonso/luksuhin-ibaba/?hotline_type=police
-
-// /api/{region}/{province}/{city_or_municipality}/
-
-// - notes:
-// - 1. fetching without any parameters will result in retrieving national emergency hotlines
-// - e.g. https://api.phemergencyhotlines.com/ <--- receive national emergency hotlines
