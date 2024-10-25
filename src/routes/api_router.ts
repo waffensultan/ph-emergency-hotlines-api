e7e1a34616950619735629717bbd41db4e86301a
Waffen Sultan <waffen.sultan@gmail.com>
2024-10-25 22:26:05 +0800

    feat: added cors

--- src/routes/api_router.ts
+++ src/routes/api_router.ts
@@ -1,15 +1,24 @@
 import express, { type Response } from "express";
+import cors from 'cors';
 
 import api_handler from "../controllers/api_handler";
 
 const api_router = express.Router();
 
+const cors_options = {
+    origin: "*",
+    "methods": "GET",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+api_router.use(cors(cors_options));
+
 api_router.get('/api/:region?/:province?/:city_or_municipality?/:barangay?', api_handler);
 api_router.get('/', (_, res: Response) => {
     res.json({
         message: "Welcome to the Philippines Emergency Hotlines API!",
-        example: "Usage: /api/{region}/{province}/{city_or_municipality}?hotline_type={hotline_type}",
-        hotline_types: "general, police, medical, rescue_disaster, earthquake_seismic, traffic, social, and digital",
+        example: "Usage: /api/{region}/{province}/{city_or_municipality}?hotline={hotline}",
+        hotline_values: "general, police, medical, rescue_disaster, earthquake_seismic, traffic, social, and digital",
         note: "Please understand that some locations might not have every type of emergency hotline available."
     })
 })
