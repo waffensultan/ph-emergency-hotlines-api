7f0fda6ea81dd36f06de8491c79f6c7e03c0692f
Waffen Sultan <waffen.sultan@gmail.com>
2024-10-29 20:19:49 +0800

    feat: updated field name

--- src/routes/api_router.ts
+++ src/routes/api_router.ts
@@ -18,7 +18,7 @@ api_router.get('/', (_, res: Response) => {
     res.json({
         message: "Welcome to the Philippines Emergency Hotlines API!",
         example: "Usage: http://ph-emergency-hotlines-api.onrender.com/api/{region}/{province}/{city_or_municipality}?hotline={hotline}",
-        hotline_values: "general, police, medical, rescue_disaster, earthquake_seismic, traffic, social, and digital",
+        hotline_type_values: "general, police, medical, rescue_disaster, earthquake_seismic, traffic, social, and digital",
         note: "Please understand that some locations might not have every type of emergency hotline available."
     })
 })
